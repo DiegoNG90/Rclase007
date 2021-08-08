26900977245923dcedfb6e51ac68561f5e8ca5c3
Diego <diego.nicolas.gandara@gmail.com>
2021-08-08 16:45:41 -0300

    Aca vemos un ejemplo de qué devuelve las props (history, match) de un route cuyo render lo pasamos como callback.

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,17 +8,29 @@ import Product from '../pages/Product/Product';
 /*
 3 formas de renderizar rutas:
 1) con prop render:
-<Route path="/" render={<Home />} />
-2)
+<Route path="/" render={Home} />
+2) con un render con callback
+<Route path="/category" render={() => <h1> Category </h1>} />
 3)
 */
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact render={<Home />} />
-        <Route path="/products" exact render={<Products />} />
-        <Route path="/product/1" render={<Product />} />
+        <Route path="/" exact render={Home} />
+        <Route path="/products" exact render={Products} />
+        <Route path="/products/:id" render={Product} />
+        <Route
+            path="/category/:id"
+            render={(props) => {
+                console.log(props)
+                return(
+                    <section>
+                        <h1> Category </h1>
+                    </section>
+                )
+            }}
+        />
       </Switch>
     </Router>
   );
