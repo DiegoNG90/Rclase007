297ece6c8a4dd4c4049f819d56eb74468972f461
Diego <diego.nicolas.gandara@gmail.com>
2021-08-08 17:07:06 -0300

    Así podemos pasarle props a ciertos componentes que nos interesa que le llegue una prop, por ejemplo, para que luego ejecute una llamada a la API con ESE value -guardado en el state search-, posiblemente pasado como endpoint query (i.e: 'http://localhost:8000/products?q=')

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,35 +4,44 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Products from '../pages/Products/Products';
 import Product from '../pages/Product/Product';
+import { useState } from 'react';
 
 /*
 3 formas de renderizar rutas:
-1) con prop render:
+1) con prop render (CUANDO QUIERO RENDERIZAR UN COMPONENTES EXERNOS E INDEPENDIENTES, SIN NECESIDAD DE PASARLE PROPS de navegacion (tipo history):
 <Route path="/" render={Home} />
 2) con un render con callback
 <Route path="/category" render={() => <h1> Category </h1>} />
-3)
+3) cuando queremos pasar una PROP a nuestro route:
+
 */
 const Routes = () => {
+  const [search, setSearch] = useState('');
+
+  const handleSearch = (e) => {
+      e.preventDefault()
+      const [product] = e.target.elements;
+      setSearch(product.value)
+  }
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact render={Home} />
-        <Route path="/products" exact render={Products} />
-        <Route path="/products/:id" render={Product} />
-        <Route
-            path="/category/:id"
-            render={(props) => {
-                console.log(props)
-                return(
-                    <section>
-                        <h1> Category </h1>
-                    </section>
-                )
-            }}
-        />
-      </Switch>
-    </Router>
+    <>
+    <form onSubmit={handleSearch}>
+      <input
+        type="text"
+        placeholder="Buscar..."
+
+      />
+      </form>
+      <Router>
+        <Switch>
+          <Route path="/" exact render={Home} />
+          <Route path="/products" exact>
+            <Products search={search} />
+          </Route>
+          <Route path="/products/:id" render={Product} />
+        </Switch>
+      </Router>
+    </>
   );
 };
 
