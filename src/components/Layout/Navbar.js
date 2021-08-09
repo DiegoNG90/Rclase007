22e89518e2cbb547f9e08d139881db8b41a28e8b
Diego <diego.nicolas.gandara@gmail.com>
2021-08-09 13:20:41 -0300

    Implementamos la funcionalidad NavLink -de react-router-dom- para la navegacion del Navbar. Tambien se agrega attributo activeClassName combinado con una clase del style.css para marcar el link activo del navbar

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,12 +1,17 @@
-import {Link} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import './style.css'
 
 const Navbar = () => {
     return (
-        <nav className="nav">
-            <Link to="/">Home</Link>
-            <Link to="/products">Products</Link>
-        </nav>
-     );
+      <nav className="nav">
+        <NavLink activeClassName="active__nav" to="/">
+          Home
+        </NavLink>
+        <NavLink activeClassName="active__nav" to="/products">
+          Products
+        </NavLink>
+      </nav>
+    );
 }
 
 export default Navbar;
